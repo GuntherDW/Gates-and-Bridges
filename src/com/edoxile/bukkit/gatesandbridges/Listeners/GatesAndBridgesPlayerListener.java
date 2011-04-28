33acafe4afa3153e48d316a8270ee07c7b13ab18
Koen Scheres <koen_75_@hotmail.com>
2011-04-28 11:27:38 +0200

    Added [x] for hidden lever control (only works with levers)

--- src/com/edoxile/bukkit/gatesandbridges/Listeners/GatesAndBridgesPlayerListener.java
+++ src/com/edoxile/bukkit/gatesandbridges/Listeners/GatesAndBridgesPlayerListener.java
@@ -4,9 +4,10 @@ import com.edoxile.bukkit.gatesandbridges.Bridge;
 import com.edoxile.bukkit.gatesandbridges.Gate;
 import com.edoxile.bukkit.gatesandbridges.GatesAndBridgesSign;
 import com.edoxile.bukkit.gatesandbridges.MechanicsType;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
-import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
@@ -50,10 +51,38 @@ public class GatesAndBridgesPlayerListener extends PlayerListener {
                     }
                 }
                 break;
-                default:
-                    return;
+                default: {
+                    Block tempBlock = event.getClickedBlock();
+                    for (int dx = -1; dx <= 1; dx++) {
+                        for (int dy = -1; dy <= 1; dy++) {
+                            for (int dz = -1; dz <= 1; dz++) {
+                                if (dx == 0 && dy == 0 && dz == 0)
+                                    continue;
+                                if (tempBlock.getRelative(dx, dy, dz).getType() == Material.SIGN_POST || tempBlock.getRelative(dx, dy, dz).getType() == Material.WALL_SIGN) {
+                                    BlockState state = tempBlock.getRelative(dx, dy, dz).getState();
+                                    if (state instanceof Sign) {
+                                        Sign sign = (Sign) state;
+                                        if (sign.getLine(1).equalsIgnoreCase("[X]")) {
+                                            for (int sdx = -1; sdx <= 1; sdx++) {
+                                                for (int sdy = -1; sdy <= 1; sdy++) {
+                                                    for (int sdz = -1; sdz <= 1; sdz++) {
+                                                        if(sdx == 0 && sdy == 0 && sdz == 0)
+                                                            continue;
+                                                        if(sign.getBlock().getRelative(sdx,sdy,sdz).getType() == Material.LEVER){
+                                                            Block lever = sign.getBlock().getRelative(sdx,sdy,sdz);
+                                                            lever.setData((byte)(lever.getData() ^ 0x8));
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
-
         }
     }
 }
